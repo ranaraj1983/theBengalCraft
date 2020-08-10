be785c099767b051888d0516ac4401e945f48b91
dasinfosolutions <ranaraj1983@gmil.com>
2020-08-10 16:35:21 +0530

    Initial check in

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_app/config/palette.dart';
+import 'package:flutter_facebook_app/screens/screens.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'The Bengal Craft',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Palette.scaffold,
+      ),
+      home: NavScreen(),
+    );
+  }
+}
